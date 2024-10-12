b798f6edbabfe3dddc4c3d82713ee2514c9a41b2
igornet0 <892130836@mail.ru>
2024-10-12 20:30:49 +0300

    Update game

--- tictactoe.c
+++ tictactoe.c
@@ -1,6 +1,8 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define MAX_SIZE 20
 #define WINNING_LENGTH 5
@@ -16,7 +18,41 @@ void initBoard() {
             board[i][j] = EMPTY;
 }
 
+
+void renderText(SDL_Renderer* renderer, const char* message, int x, int y, SDL_Color color) {
+    TTF_Font* font = TTF_OpenFont("arial.ttf", 24);
+    if (!font) {
+        printf("Failed to load font: %s\n", TTF_GetError());
+        return;
+    }
+
+    SDL_Surface* surface = TTF_RenderText_Solid(font, message, color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_Rect rect = {x, y, surface->w, surface->h};
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+    TTF_CloseFont(font);
+}
+
+
+int checkDraw() {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (board[i][j] == EMPTY) {
+                return 0; // Есть пустая клетка, продолжаем игру.
+            }
+        }
+    }
+    return 1; // Все клетки заполнены, ничья.
+}
+
+
 void drawBoard(SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
     for (int i = 0; i <= width; i++) {
         SDL_RenderDrawLine(renderer, i * 50, 0, i * 50, height * 50);
     }
@@ -27,24 +63,21 @@ void drawBoard(SDL_Renderer* renderer) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             if (board[i][j] == PLAYER_X) {
-                // Draw X
                 SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
                 SDL_RenderDrawLine(renderer, j * 50 + 10, i * 50 + 10, j * 50 + 40, i * 50 + 40);
                 SDL_RenderDrawLine(renderer, j * 50 + 40, i * 50 + 10, j * 50 + 10, i * 50 + 40);
             } else if (board[i][j] == PLAYER_O) {
-                // Draw O
-                SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-                for (int r = 0; r < 50; r++) {
-                    for (int angle = 0; angle < 360; angle++) {
-                        int x = r * cos(angle * M_PI / 180) + j * 50 + 25;
-                        int y = r * sin(angle * M_PI / 180) + i * 50 + 25;
-                        SDL_RenderDrawPoint(renderer, x, y);
-                    }
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                for (int angle = 0; angle < 360; angle++) {
+                    int x = 20 * cos(angle * M_PI / 180) + j * 50 + 25;
+                    int y = 20 * sin(angle * M_PI / 180) + i * 50 + 25;
+                    SDL_RenderDrawPoint(renderer, x, y);
                 }
             }
         }
     }
 }
+
 
 int checkWin(Cell player) {
     // Check horizontal, vertical and diagonal for winning condition
@@ -85,7 +118,6 @@ int checkWin(Cell player) {
 }
 
 void aiMove() {
-    // Simple AI that makes a random move
     int x, y;
     do {
         x = rand() % height;
@@ -98,12 +130,13 @@ void aiMove() {
 int main(int argc, char* argv[]) {
     printf("Enter width and height of the board (max %d): ", MAX_SIZE);
     scanf("%d %d", &width, &height);
-        if (width > MAX_SIZE || height > MAX_SIZE || width < 1 || height < 1) {
-        printf("Invalid size!n");
+    if (width > MAX_SIZE || height > MAX_SIZE || width < 1 || height < 1) {
+        printf("Invalid size!\n");
         return -1;
     }
 
     SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init(); // Инициализация TTF для отображения текста
     SDL_Window* window = SDL_CreateWindow("Tic Tac Toe", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width * 50, height * 50, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
@@ -111,23 +144,43 @@ int main(int argc, char* argv[]) {
     
     int running = 1;
     Cell currentPlayer = PLAYER_X;
+    int gameOver = 0;
+    char message[50] = "";
 
     while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) running = 0;
-            if (event.type == SDL_MOUSEBUTTONDOWN && currentPlayer == PLAYER_X) {
+
+            if (event.type == SDL_MOUSEBUTTONDOWN && !gameOver) {
                 int x = event.button.x / 50;
                 int y = event.button.y / 50;
 
                 if (x < width && y < height && board[y][x] == EMPTY) {
                     board[y][x] = PLAYER_X;
-                    if (!checkWin(PLAYER_X)) {
+                    if (checkWin(PLAYER_X)) {
+                        snprintf(message, sizeof(message), "Player X wins!");
+                        gameOver = 1;
+                    } else if (checkDraw()) {
+                        snprintf(message, sizeof(message), "It's a draw!");
+                        gameOver = 1;
+                    } else {
                         currentPlayer = PLAYER_O;
                         aiMove();
+                        if (checkWin(PLAYER_O)) {
+                            snprintf(message, sizeof(message), "Player O wins!");
+                            gameOver = 1;
+                        } else if (checkDraw()) {
+                            snprintf(message, sizeof(message), "It's a draw!");
+                            gameOver = 1;
+                        }
                         currentPlayer = PLAYER_X;
                     }
                 }
+            }
+
+            if (gameOver && (event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_KEYDOWN)) {
+                running = 0; // Закрываем игру при нажатии клавиши или клике
             }
         }
 
@@ -135,20 +188,18 @@ int main(int argc, char* argv[]) {
         SDL_RenderClear(renderer);
         
         drawBoard(renderer);
-        
-        SDL_RenderPresent(renderer);
 
-        if (checkWin(PLAYER_X)) {
-            printf("Player X wins!n");
-            running = 0;
-        } else if (checkWin(PLAYER_O)) {
-            printf("Player O wins!n");
-            running = 0;
+        if (gameOver) {
+            SDL_Color color = {0, 0, 0, 255}; // Черный цвет текста
+            renderText(renderer, message, 10, height * 50 / 2 - 20, color);
         }
+
+        SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_Quit();
     SDL_Quit();
 
     return 0;
