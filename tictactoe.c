b18ec19d5834fddfacfff6213d64318c34641ed4
igornet0 <892130836@mail.ru>
2024-10-12 21:15:39 +0300

    Fix game

--- tictactoe.c
+++ tictactoe.c
@@ -140,14 +140,14 @@ int checkWin(Cell player) {
                     board[i + 3][j] == player && 
                     board[i + 4][j] == player) return 1;
 
-                // Проверка диагонали \
+                // Проверка диагонали 
                 if (i <= MAX_SIZE - WINNING_LENGTH && j <= MAX_SIZE - WINNING_LENGTH &&
                     board[i + 1][j + 1] == player && 
                     board[i + 2][j + 2] == player && 
                     board[i + 3][j + 3] == player && 
                     board[i + 4][j + 4] == player) return 1;
 
-                // Проверка диагонали /
+                // Проверка диагонали 
                 if (i >= WINNING_LENGTH - 1 && j <= MAX_SIZE - WINNING_LENGTH &&
                     board[i - 1][j + 1] == player && 
                     board[i - 2][j + 2] == player && 
@@ -174,7 +174,7 @@ int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
     SDL_Window* window = SDL_CreateWindow("Infinite Tic Tac Toe", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     initBoard();
     
